5153a6cc937052eb9bccf1d98e04aa0d977420d4
pmaxis <code.developer0601@gmail.com>
2022-08-08 15:38:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ data.js
@@ -0,0 +1,627 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-----",
+      "name": "Панорама будівлі з боку парку",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": -0.3427406799315573,
+        "pitch": 0.050952816760958086,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11713866212808277,
+          "pitch": 0.1793086920620084,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": 0.3642861012186014,
+          "pitch": 0.1762342976458946,
+          "rotation": 0.7853981633974483,
+          "target": "13-------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Центральний вхід",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.021302950060199,
+          "pitch": 0.41797045012758005,
+          "rotation": 0,
+          "target": "0-----"
+        },
+        {
+          "yaw": -0.04732684942102949,
+          "pitch": 0.3990860643193308,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Центральний вхід",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": -0.6009550693196868,
+        "pitch": -0.028569924095080523,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3445160479276055,
+          "pitch": 0.2589702558022786,
+          "rotation": 0,
+          "target": "3----"
+        },
+        {
+          "yaw": -3.111982361260541,
+          "pitch": 0.7539230177525535,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3----",
+      "name": "Центральний вхід до музею",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05368104740182211,
+          "pitch": 0.28104931476161354,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": 3.0251456600389037,
+          "pitch": 0.34810367975929424,
+          "rotation": 0,
+          "target": "3----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "Центральний вхід",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": 0.6925216564861962,
+        "pitch": 0.08355753752500128,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2824239986702679,
+          "pitch": 0.7027495504967938,
+          "rotation": 11.780972450961727,
+          "target": "5------"
+        },
+        {
+          "yaw": 1.6478930647825667,
+          "pitch": 0.3783159393087381,
+          "rotation": 0,
+          "target": "3----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5------",
+      "name": "Вигляд музею зі сторони р. Тетерів",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": -0.14601565196419308,
+        "pitch": -0.2285593927606442,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6592070187375167,
+          "pitch": 0.2081816786480708,
+          "rotation": 5.497787143782138,
+          "target": "6------"
+        },
+        {
+          "yaw": 1.1752768062186334,
+          "pitch": 0.38626936925873423,
+          "rotation": 5.497787143782138,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6------",
+      "name": "Вид на музей зі сторони альтанки",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3665429448565813,
+          "pitch": 0.14383999833089867,
+          "rotation": 6.283185307179586,
+          "target": "7-----"
+        },
+        {
+          "yaw": 1.5414473961733233,
+          "pitch": 0.2872605302757858,
+          "rotation": 0,
+          "target": "5------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-----",
+      "name": "Вікна, через які рятували експонати",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": -0.30740119490676143,
+        "pitch": -0.03789581903080297,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1983731720944437,
+          "pitch": 0.168801385417785,
+          "rotation": 0.7853981633974483,
+          "target": "8---"
+        },
+        {
+          "yaw": 1.607074218605348,
+          "pitch": 0.34991387736518753,
+          "rotation": 0,
+          "target": "6------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8---",
+      "name": "Місце влучання снаряду",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": -0.24454300896841552,
+        "pitch": -0.010454192847534571,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4896910933555674,
+          "pitch": 0.14677809740136283,
+          "rotation": 0,
+          "target": "9---"
+        },
+        {
+          "yaw": 1.144943399842381,
+          "pitch": 0.283425427968508,
+          "rotation": 5.497787143782138,
+          "target": "7-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9---",
+      "name": "Місце влучання снаряду",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8727009211123509,
+          "pitch": 0.1406924000575529,
+          "rotation": 0.7853981633974483,
+          "target": "10------"
+        },
+        {
+          "yaw": 1.4151717516507336,
+          "pitch": 0.25439035607207217,
+          "rotation": 0,
+          "target": "8---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10------",
+      "name": "Вид на музей із західної сторони",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": 0.6014690518044912,
+        "pitch": 0.055354227934218514,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2390187157336321,
+          "pitch": 0.2314631086857002,
+          "rotation": 5.497787143782138,
+          "target": "11-----"
+        },
+        {
+          "yaw": 2.5624193466753162,
+          "pitch": 0.27341675040421975,
+          "rotation": 5.497787143782138,
+          "target": "9---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-----",
+      "name": "Вигляд музею із західної сторони",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6068486594248732,
+          "pitch": 0.19006159128645095,
+          "rotation": 10.995574287564278,
+          "target": "12-----"
+        },
+        {
+          "yaw": 2.009962052046541,
+          "pitch": 0.290077481790874,
+          "rotation": 0,
+          "target": "10------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-----",
+      "name": "Вигляд музею із західної сторони",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": 1.1040366019896837,
+        "pitch": -0.062496708957974434,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6640988544748172,
+          "pitch": 0.3782770436036387,
+          "rotation": 11.780972450961727,
+          "target": "13-------"
+        },
+        {
+          "yaw": 2.6669740545936573,
+          "pitch": 0.2356053881563298,
+          "rotation": 0,
+          "target": "11-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-------",
+      "name": "Панорама центрального входу із західної сторони ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": -2.1009776264653635,
+        "pitch": 0.17498437495439845,
+        "fov": 1.5034922555036627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0625011049412816,
+          "pitch": 0.3429403778475333,
+          "rotation": 5.497787143782138,
+          "target": "0-----"
+        },
+        {
+          "yaw": -1.5618362579911285,
+          "pitch": 0.3936814946414877,
+          "rotation": 0,
+          "target": "12-----"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
